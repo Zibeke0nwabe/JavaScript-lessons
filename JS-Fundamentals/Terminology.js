653731077e7c8811cd276cb5eb330edc6f4dcf11
Zibeke Onwabe <lizoxolo9@gmail.com>
2024-09-12 09:44:15 +0200

    first commit of terminologies

---
+++ JS-Fundamentals/Terminology.js
@@ -0,0 +1,23 @@
+/**
+        USING THE TERMINOLOGIES TO WORK ON CODING CHALLENGES
+ */
+
+/**
+    Write a JavaScript statement to declare a variable name with a 
+    value of Zibeke Onwabe
+ */
+var name = 'Zibeke Onwabe';
+
+/**
+ * what is the difference between null and undifined in JavaScript?
+ * Write an example to demonstrate the difference
+ * 
+ * null value is a empty values which a developer assigned intentionally 
+ * whereas a undefined is a when a name is declared without a value
+ */
+
+    var x = null; //null value which I have given the value of null
+    var y; // undefined value
+
+    console.log(x); // will give a null value
+    console.log(y); // will give undefined
